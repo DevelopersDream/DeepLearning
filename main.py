dc919c72ad01448bff16445718a095c87b61b94f
DevelopersDream <mortella.2058262@studenti.uniroma1.it>
2024-12-04 23:59:34 +0100

    base pre-processing
    computed statistics for the dataset. Lowercased strings, titles and texts united. Single list of documents is now available.

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 import constants as c
-from utils import one_json
+import utils as u
 
 
-data = one_json()
+data = u.one_json()
 
 
-print(data[2])
+data = data[1:] #first document is not in sicilian, TODO also some other...
 
+dataset_statistics = u.compute_init_dataset_stats(data)
+
+doc_list = []
+
+for elem in data: #data pre-processing and cleaning
+
+    doc_list.append([elem["title"].lower()])  # no /n in titles, converting the single string in a list
+
+    if elem["text"] != "":
+        doc_list.append(elem["text"].lower().split("\n")) # a different docuemnt for every /n
+
+doc_list = [item for sublist in doc_list for item in sublist] #list of lists -> list
+
+del data
+
+dataset_statistics = u.compute_final_dataset_stats(dataset_statistics, doc_list)
+
+print(dataset_statistics)
+
+
+
+# for elem in data:
+#  print(elem)
+
+
+# for elem in data:
+# print(elem["id"])
+
+# data = [elem for elem in data if elem["title"] == "Tiatru"] #select a page based on the title
+
+# text_list = [elem for elem in text_list if elem.count(" ")>0] #remove uni-grams
+
+# for elem in text_list:
+# print(elem.count(" "))
+# print(elem)
--- utils.py
+++ utils.py
@@ -16,3 +16,31 @@ def one_json() -> list:
                 data.append(json.loads(line))
 
     return data
+
+
+def compute_init_dataset_stats(data: list) -> dict:
+    """
+    compute statistics for the initial dataset
+
+    """
+    dataset_statistics = {}
+    dataset_statistics["initial_documents"] = len(data)*2 #1 title and 1 text
+    dataset_statistics["initial_textWords_average"] = sum(len(elem["text"].split()) for elem in data) / len(data)
+    dataset_statistics["initial_text_maxWordsNum"] = max(len(elem["text"].split()) for elem in data)
+    dataset_statistics["initial_text_nullPerc"] = sum(1 for d in data if len(d.get("text", "")) == 0)*100 / len(data)
+    dataset_statistics["initial_titleWords_average"] = sum(len(elem["title"].split()) for elem in data) / len(data)
+    dataset_statistics["initial_title_maxWordsNum"] = max(len(elem["title"].split()) for elem in data)
+    dataset_statistics["initial_title_nullPerc"] = sum(1 for d in data if len(d.get("title", "")) == 0)*100 / len(data)
+    return dataset_statistics
+
+
+def compute_final_dataset_stats(dataset_statistics: list, doc_list: list) -> list:
+    """
+    compute statistics for the initial dataset
+
+    """
+    dataset_statistics["final_documents"] = len(doc_list)
+    dataset_statistics["final_textWords_average"] = sum(len(elem.split()) for elem in doc_list) / len(doc_list)
+    dataset_statistics["final_text_maxWordsNum"] = max(len(elem.split()) for elem in doc_list)
+
+    return dataset_statistics
